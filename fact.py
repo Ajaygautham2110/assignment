20424a51afb23d023eaee208a9972c204f888e16
Ajaygautham2110 <101620150+Ajaygautham2110@users.noreply.github.com>
2022-03-16 15:57:13 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,12 @@
+num = int(input("enter the number : "))
+factorial = 1
+if num < 0 :
+    print("enter correct number")
+
+elif num == 0 :
+    print("factorial of",num,"is 1")
+    
+else:
+    for  range(1,num + 1):
+        factorial = factorial*i
+    print("factorial of",num,"is ",factorial)
